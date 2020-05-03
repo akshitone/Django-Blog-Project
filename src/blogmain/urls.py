57fa974adcf707c787a0d09e9baccb014e15a39a
Akshit Mithaiwala <akshitone8@gmail.com>
2020-05-03 11:46:10 +0530

    master

--- src/blogmain/urls.py
+++ src/blogmain/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_page
+from .views import home_page,aboutus_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page),
-    path('about/', home_page),
+    path('aboutus/', aboutus_page),
     path('contact/', home_page),
 ]
--- src/blogmain/views.py
+++ src/blogmain/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+# def home_page(request):
+#     return HttpResponse("Hello world")
 
 def home_page(request):
-    return HttpResponse("Hello world")
+    return render(request, "index.html")
+
+def aboutus_page(request):
+    return render(request, "aboutus.html")
